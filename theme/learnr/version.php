1ec372d90ac5a727c85d0518f732d8cf42a04185
Gael Donat <gael.donat@recommerce.com>
2024-02-05 07:40:19 +0000

    add CI/CD + theme

---
+++ theme/learnr/version.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme Boost Union - Version file
+ *
+ * @package    theme_learnr
+ * @copyright  2022 Alexander Bias, lern.link GmbH <alexander.bias@lernlink.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'theme_learnr';
+$plugin->version = 2023102000;
+$plugin->release = 'v4.3-r1';
+$plugin->requires = 2023100900;
+$plugin->supported = [403, 403];
+$plugin->maturity = MATURITY_STABLE;
+$plugin->dependencies = ['theme_boost' => 2023100900];
